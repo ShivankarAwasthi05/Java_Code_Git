455b3c069c9f8b2fa70ab96538d31b796c9f3653
Shivankar Awasthi <152833921+ShivankarAwasthi05@users.noreply.github.com>
2025-02-26 04:35:31 +0000

    container commit done

---
+++ StacksOperations/BasicsOfStack2.java
@@ -0,0 +1,12 @@
+package StacksOperations;
+import java.util.Stack;
+public class BasicsOfStack2 {
+    public static  void main(String args[]){
+    Stack<String> st=new Stack<String>();
+    st.push("RadhaRani");
+    st.push("Shriji");
+    st.push("Shyamashyam");
+    st.push("Ramya");
+    System.out.println(st);
+    }
+}
