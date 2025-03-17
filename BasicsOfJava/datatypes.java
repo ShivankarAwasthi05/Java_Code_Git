e29f144346b2b8dc899b28ff52636d1acc9a7fcc
Shivankar Awasthi <152833921+ShivankarAwasthi05@users.noreply.github.com>
2025-03-17 09:29:11 +0000

    container config done

---
+++ BasicsOfJava/datatypes.java
@@ -0,0 +1,28 @@
+package BasicsOfJava;
+
+public class datatypes {
+    public static void main(String args[]){
+    int a=1;
+    System.out.println(Integer.toString(a));
+    String name="Radha";
+    System.out.println(name);
+    }
+}
+/*
+  Datatypes in java
+  1)int 
+  2)char
+  3)byte
+  4)short
+  5)double
+  6)float
+ */
+
+ /*
+  wrappers
+
+  String : "a"+"b"="ab"
+  Integer:int
+  Long
+*/
+
