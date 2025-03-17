a17605b4f73a1eb915de30a2212ffc08c56eb521
Shivankar Awasthi <152833921+ShivankarAwasthi05@users.noreply.github.com>
2025-03-17 09:17:45 +0000

    container config done

--- BasicsOfJava/basics.java
+++ BasicsOfJava/basics.java
@@ -2,7 +2,14 @@ package BasicsOfJava;
 
 public class basics {
     public static void main(String args[]){
-       System.out.println("RadhaVallabhShriHariVansh");
+       System.out.println("RadhaVallabhShriHariVansh 1");
+       System.out.println(args[0]);
+       System.out.println(args[1]);
+    }
+     public static void main1(String args[]){
+       System.out.println("RadhaVallabhShriHariVansh 2");
+    }
+    public static void main2(String args[]){
+       System.out.println("RadhaVallabhShriHariVansh 3");
     }
 }
-
